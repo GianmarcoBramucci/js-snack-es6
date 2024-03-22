4909219da9dfb0c1601e8a769f4d007af0c5dd8f
GianmarcoBramucci <giangian797@gmail.com>
2024-03-22 15:39:25 +0100

    first snack

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+//! primo snack
 let invitati = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+let listaCompleta = invitati.map((invitato,i) =>{
+    let persona = {
+        tableName : 'Tavolo vip',
+        guestName : invitato,
+        place : i
+    }
+    return persona;
+});
+
+console.log(listaCompleta);
+
