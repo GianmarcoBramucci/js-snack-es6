eb3e47bd758aaae389206840fc2b9e4149cfa413
GianmarcoBramucci <giangian797@gmail.com>
2024-03-22 15:57:27 +0100

    second snack

--- js/main.js
+++ js/main.js
@@ -11,3 +11,62 @@ let listaCompleta = invitati.map((invitato,i) =>{
 
 console.log(listaCompleta);
 
+//! secondo snack
+let aula = [
+    {
+        id: '213',
+        name: 'Marco della Rovere',
+        grades: '78',
+    },
+    {
+        id: '110',
+        name: 'Paola Cortellessa',
+        grades: '96',
+    },
+    {
+        id: '250',
+        name: 'Andrea Mantegna',
+        grades: '48',
+    },
+    {
+        id: '145',
+        name: 'Gaia Borromini',
+        grades: '74',
+    },
+    {
+        id: '196',
+        name: 'Luigi Grimaldello',
+        grades: '68',
+    },
+    {
+        id: '102',
+        name: 'Piero della Francesca',
+        grades: '50',
+    },
+    {
+        id: '120',
+        name: 'Francesca da Polenta',
+        grades: '84',
+    },
+];
+
+for(let studente of aula){
+    studente.name = studente.name.toUpperCase();
+}
+
+console.log(aula);
+
+arraySecond = aula.filter((studente) =>{
+    if(parseInt(studente.grades) >70){
+        return true;
+    }
+});
+
+arrayTerzo = aula.filter((studente) =>{
+    if(parseInt(studente.grades) >70 && parseInt(studente.id) >120){
+        return true;
+    }
+});
+
+console.log(arraySecond);
+console.log(arrayTerzo);
